53f8c89918bdf0a5545eb07bf0b3fcc9cfc2f1a0
hannahlef <31733972+hannahlef@users.noreply.github.com>
2019-04-22 10:53:16 -0400

    Update visualizeEEG.m

--- EEG/visualizeEEG.m
+++ EEG/visualizeEEG.m
@@ -1,10 +1,10 @@
-function visualizeEEG (label,freq)
+function visualizeEEG ()
 
 nbCh = length(label)+1;
 
 
 vis_stream('streamname','OpenBCI_EEG', 'bufferrange', 20 , 'timerange', 5,'DataScale',100, 'ChannelRange',1:nbCh,...
-   'SamplingRate',freq, 'RefreshRate',10,'FrequencyFilter',[4 5 50 60],'ZeroMean',true, 'channellabel', label);
+   'SamplingRate',freq, 'RefreshRate',10,'FrequencyFilter',[4 5 50 60],'ZeroMean',true);
 
 
 end
